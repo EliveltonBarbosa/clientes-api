d6374ca7777e7b253720c54cfaa71ef2b66e266c
Elivelton Barbosa <barbosa.elivelton22@gmail.com>
2024-05-25 11:05:32 -0300

    Implementing CPF validator and fixing nome validator

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -11,7 +11,7 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': 'Não inclua números neste campo.'})
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 dígitos.'})
+            raise serializers.ValidationError({'cpf': 'O CPF deve ser válido, com 11 dígitos (sem pontos e hífen).'})
         if not rg_valido(data['rg']): 
             raise serializers.ValidationError({'rg': 'O RG deve ter exatamente 9 dígitos.'})
         if not celular_valido(data['celular']):
